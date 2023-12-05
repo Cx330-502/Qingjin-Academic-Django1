4339c9e70fc31d64fd0526c67a7885b265ac3c74
Cx330-502 <1175963382@qq.com>
2023-12-05 21:22:34 +0800

    更新 settings.py

--- Qingjin_Academic/Qingjin_Academic/settings.py
+++ Qingjin_Academic/Qingjin_Academic/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+t&g5^vtw_7a123re*@90bbvaw4l@0yy_mid0*^^716qtppydd"
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
